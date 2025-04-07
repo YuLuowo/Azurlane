ae3302c63b9f12e32ff2754bd7249e52da0ab232
YuLuowo <diego050415@gmail.com>
2025-04-07 18:56:20 +0800

    Update page.tsx

--- src/app/ships/[shipName]/page.tsx
+++ src/app/ships/[shipName]/page.tsx
@@ -1,11 +1,10 @@
-import { FC } from "react";
-
 interface ShipPageProps {
     params: { shipName: string };
 }
 
-const ShipPage: FC<ShipPageProps> = ({ params }) => {
+const ShipPage = async ({ params }: ShipPageProps) => {
     const decodedShipName = decodeURIComponent(params.shipName);
+
     return (
         <div className="flex h-screen items-center justify-center bg-gray-100">
             <h1 className="text-3xl font-bold text-blue-600">{decodedShipName}</h1>
